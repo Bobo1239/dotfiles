0e7d2077aefa967d7f175aad81c6cdcd52cfc221
Boris-Chengbiao Zhou <bobo1239@web.de>
2019-12-15 05:07:14 +0100

    Add pacman-update-mirrorlist script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-# Theoretically the syntax is incompatbile; I'm only exporting environment variables so whatever
+# Theoretically the syntax is incompatible; I'm only exporting environment variables so whatever
 source ~/.profile
 
 if not set -q DISPLAY && test (tty) = /dev/tty1;
