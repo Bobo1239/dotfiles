a0c1ff9c7c62c9c4084ac00b893f24111e653e2d
Boris-Chengbiao Zhou <bobo1239@web.de>
2019-12-16 15:42:09 +0100

    fish: Don't autostart sway on server

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 # Theoretically the syntax is incompatible; I'm only exporting environment variables so whatever
 source ~/.profile
 
-if not set -q DISPLAY && test (tty) = /dev/tty1;
+if not set -q DISPLAY && test (tty) = /dev/tty1 && not contains (hostname) "SERVER";
     exec sway;
 end
