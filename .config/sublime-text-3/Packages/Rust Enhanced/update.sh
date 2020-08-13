6c9434d63bfbb0959a9ca5d72c880f3f46408444
Boris-Chengbiao Zhou <bobo1239@web.de>
2020-08-13 15:29:53 +0200

    st3: Update syntax checking hack to use build-std instead of cargo-xbuild

--- .config/sublime-text-3/Packages/Rust Enhanced/update.sh
+++ .config/sublime-text-3/Packages/Rust Enhanced/update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 unzip -p ../../Installed\ Packages/Rust\ Enhanced.sublime-package rust/cargo_settings.py > rust/cargo_settings.py
-patch -p1 < xbuild.patch
+patch -p1 < build-std.patch
